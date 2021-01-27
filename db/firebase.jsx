325c5fc66e6cfb2109cce4ca656b8be46d47bbce
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-01-27 11:10:30 -0500

    commit to deploy to vercel

--- db/firebase.jsx
+++ db/firebase.jsx
@@ -1,8 +1,20 @@
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+  console.log('private key', process.env.PRIVATE_KEY)
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    // credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert({
+      type: process.env.TYPE,
+      projectId: process.env.PROJECT_ID,
+      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.CLIENT_EMAIL,
+      clientId: process.env.CLIENT_ID,
+      authUri: process.env.AUTH_URI,
+      tokenUri: process.env.TOKEN_URI,
+      authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+      clientX509CertUrl: process.env.CLIENT_X509_CERT_URL
+    }),
     databaseURL: 'https://inventory-aac80.firebaseio.com'
   });
   admin.firestore().settings({ ignoreUndefinedProperties: true })
