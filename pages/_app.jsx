077560e6a9dbab46d3b8e544dad0cfac1f6b9c57
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-02-05 16:57:48 -0500

    updated styling syntax to pseudo-scss

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -44,17 +44,6 @@ const MyApp = ({ Component, pageProps }) => {
     <Layout>
       <Head>
         <title>Inventory</title>
-        <meta name="description" content="Home inventory management system for workshop" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-        <meta name="apple-mobile-web-app-title" content="Inventory" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="HandheldFriendly" content="true" />
-        <meta name="MobileOptimized" content="width" />
-
-        <link rel="apple-touch-icon" type="image/png" href='/icons/apple-touch-icon.png' />
-        <link rel="icon" type="image/svg" href='/hand-receiving-solid.svg' />
-        <link rel="manifest" href='/manifest.json' />
       </Head>
       <Component {...pageProps} />
       <Loading loading={loading} />
